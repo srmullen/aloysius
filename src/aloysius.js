76b9debe10cb273d3017d83c59c039fbea8f81fc
Sean Mullen <srmullen13@gmail.com>
2015-08-05 19:30:30 -0400

    initial commit

---
+++ src/aloysius.js
@@ -0,0 +1,5 @@
+function consonant (n1, n2) {
+
+}
+
+export {consonant};
---
+++ test/aloyisiusSpec.js
@@ -0,0 +1,10 @@
+import {expect} from "chai";
+import {consonant} from "../src/aloysius";
+
+describe("aloysius", () => {
+    describe("consonant", () => {
+        it("should exist", () => {
+            expect(consonant).to.be.defined;
+        });
+    });
+});
